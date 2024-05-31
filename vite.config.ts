1e950ef81aa0b89e8bf29d7fcfa18895fa73ec61
Lorenzo Tomas Diez <lorenzotomasdiez@Lorenzos-MacBook-Air.local>
2024-05-31 15:56:13 -0300

    adds a only js

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
 import tailwindcss from "tailwindcss";
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,7 +28,11 @@ export default defineConfig({
     sourcemap: true,
     emptyOutDir: true,
   },
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [
+    react(),
+    dts({ rollupTypes: true }),
+    cssInjectedByJsPlugin()
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
